c72a58a7f4b4a2a4d91a1b23040feeb34694c221
affixusimac1 <viveking@outlook.com>
2018-10-10 12:41:15 +0530

    Commit

--- CapsPageMenu/ViewController.swift
+++ CapsPageMenu/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import PageMenu
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CAPSPageMenuDelegate {
+    
+    @IBOutlet weak var redView: UIView!
+    
+    var aPageMenu : CAPSPageMenu?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+         self.view.addSubview(redView)
+        
+        var controllerArray : [UIViewController] = []
+        
+        let profileVC: UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "vc1")
+       profileVC.title = "VC 1"
+        controllerArray.append(profileVC)
+        
+        let scheduleVC: UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "vc2")
+        scheduleVC.title = "VC 2"
+        controllerArray.append(scheduleVC)
+        
+        let parameters: [CAPSPageMenuOption] = [
+            .menuItemSeparatorWidth(4.3),
+            .useMenuLikeSegmentedControl(true),
+            .menuItemSeparatorPercentageHeight(0.1)
+        ]
+        
+        aPageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRect(x: 0.0,y: 0.0, width: self.view.frame.width,height: self.view.frame.height), pageMenuOptions: parameters)
+        aPageMenu!.delegate = self
+        self.view.addSubview(aPageMenu!.view)
+//
+//        viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
